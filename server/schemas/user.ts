1845349ecfb53db6c55e8f5e16f0febdb71f0da3
Yousef <yousefsameh24@gmail.com>
2024-04-08 14:59:16 +0200

    create user schema

---
+++ server/schemas/user.ts
@@ -0,0 +1,101 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import User from '../models/user'
+import { GraphQLError } from 'graphql'
+import 'dotenv/config'
+
+const typeDefs = `
+    type User {
+        name: String!
+        username: String!
+        boards: [Board!]!
+        id: ID!
+    }
+
+    type Token {
+        value: String!
+    }
+
+    extend type Query {
+        me: User
+    }
+
+    extend type Mutation {
+        createUser(
+            name: String!
+            username: String!
+            password: String!
+        ): User
+
+        login(
+            username: String!
+            password: String!
+        ): Token
+    }
+`
+
+const resolvers = {
+    Query: {
+        me: (root, args, context) => {
+            const currentUser = context.currentUser
+            if (!currentUser) {
+                throw new GraphQLError('Not authenticated', {
+                    extensions: {
+                        code: 'UNAUTHENTICATED'
+                    }
+                })
+            }
+            return currentUser.populate('boards')
+        }
+    },
+    Mutation: {
+        createUser: async (root, args) => {
+            const saltRounds = 10
+            const passwordHash = await bcrypt.hash(args.password, saltRounds)
+
+            const user = new User({
+                name: args.name,
+                username: args.username,
+                passwordHash
+            })
+
+            try {
+                await user.save()
+            } catch (error) {
+                if(error.code === 11000) {
+                    throw new GraphQLError('Username is already taken', {
+                        extensions: {
+                            code: 'BAD_USER_INPUT'
+                        }
+                    })
+                }
+                throw new GraphQLError(error.message)
+            }
+
+            return user
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username })
+            const passwordCorrect = user === null
+                ? false
+                : await bcrypt.compare(args.password, user.passwordHash)
+
+            if (!(user && passwordCorrect)) {
+                throw new GraphQLError('invalid username or password', {
+                    extensions: {
+                        code: 'UNAUTHENTICATED'
+                    }
+                })
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id
+            }
+
+            return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+        }
+    }
+}
+
+export default { typeDefs, resolvers }
