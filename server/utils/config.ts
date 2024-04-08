833d41209372c3817bd66a4a3602a6badfb7ad38
Yousef <yousefsameh24@gmail.com>
2024-04-08 14:59:31 +0200

    create config

---
+++ server/utils/config.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.NODE_ENV === 'development'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+const FRONTEND_URL = process.env.NODE_ENV === 'development'
+  ? process.env.DEV_FRONTEND_URL
+  : process.env.PROD_FRONTEND_URL
+
+export default {
+    PORT,
+    MONGODB_URI,
+    FRONTEND_URL
+}
