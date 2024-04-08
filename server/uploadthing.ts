32d5ec279444c8f9156b3620f0e19afbb2073713
Yousef <yousefsameh24@gmail.com>
2024-04-08 12:34:58 +0200

    create route handler

---
+++ server/uploadthing.ts
@@ -0,0 +1,24 @@
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+import 'dotenv/config'
+
+const f = createUploadthing({
+  errorFormatter: (err) => {
+    console.log("Error uploading file", err.message);
+    console.log("  - Above error caused by:", err.cause);
+
+    return { message: err.message };
+  },
+});
+
+export const uploadRouter = {
+  imageUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+  }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof uploadRouter;
