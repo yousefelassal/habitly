192fab39303f1ba683182a8e05a7c07bf5d1fe4e
Yousef <yousefsameh24@gmail.com>
2024-04-09 13:28:18 +0200

    use provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ApolloProvider from './hooks/ApolloProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ApolloProvider>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
 )
