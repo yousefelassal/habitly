040e5e55f198117ffa8edbea0377823e94989b2e
Yousef <yousefsameh24@gmail.com>
2024-04-09 14:36:19 +0200

    Refactor App component to use React Router for client-side routing and add authentication functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,43 @@ import {
   Navigate,
   useLocation
 } from "react-router-dom"
+import { useApolloClient } from "@apollo/client"
 import { useAuth } from "./hooks/useAuth"
 import Home from "./pages/Home"
+import LandingPage from "./pages/LandingPage"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 import Header from "./components/Header"
+import useHasMounted from "./hooks/useHasMounted"
 
 function App() {
-  const { user, logout } = useAuth()
+  const { user } = useAuth()
+  const client = useApolloClient()
   const location = useLocation()
   const prevLocation = location.state?.prevLocation
+  const hasMounted = useHasMounted()
 
-  if (!user) {
-    return <Navigate to="/login" replace={true} />
+  const logout = () => {
+    user.setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!hasMounted) {
+    return null
+  }
+
+  if (!user.token) {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login setUser={user.setToken} />} />
+          <Route path="/signup" element={<Signup setUser={user.setToken} />} />
+          <Route path="/*" element={<Navigate to="/" replace={true}/>} />
+        </Routes>
+      </>
+    )
   }
 
   return (
@@ -23,6 +49,7 @@ function App() {
       <Routes location={prevLocation || location}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Navigate to="/" replace={true} />} />
+        <Route path="/signup" element={<Navigate to="/" replace={true} />} />
       </Routes>
     </>
   )
