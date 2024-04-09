8e6d00a817adfd7d19cc06a23c71fe8baa91e472
Yousef <yousefsameh24@gmail.com>
2024-04-09 14:36:29 +0200

    Refactor main.tsx to use JotaiProvider for state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import ApolloProvider from './hooks/ApolloProvider.tsx'
+import { Provider as JotaiProvider } from 'jotai'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider>
-      <Router>
-        <App />
-      </Router>
-    </ApolloProvider>
+    <JotaiProvider>
+      <ApolloProvider>
+        <Router>
+          <App />
+        </Router>
+      </ApolloProvider>
+    </JotaiProvider>
   </React.StrictMode>,
 )
