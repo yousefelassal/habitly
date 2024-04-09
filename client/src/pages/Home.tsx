bfe8c956fca99101ff40156e84cd6576643270db
Yousef <yousefsameh24@gmail.com>
2024-04-09 14:38:07 +0200

    Refactor Home component to use Apollo Client for GraphQL API integration and add loading and error handling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,31 @@
-
+import { useQuery } from "@apollo/client"
+import {
+    HabitsDocument,
+    HabitsQuery,
+    HabitsQueryVariables,
+} from '@/generated/graphql'
+import { useDocumentTitle } from "@uidotdev/usehooks"
 
 const Home = () => {
+  const { data, loading, error } = useQuery<HabitsQuery, HabitsQueryVariables>(HabitsDocument)
+  useDocumentTitle('Home | Habitly')
   return (
-    <div>Home</div>
+    <>
+    {loading && <p>Loading...</p>}
+    {error && <p>Error</p>}
+    {data && (
+        <div>
+            <h1>Home</h1>
+            <ul>
+                {data.allHabits.map((habit) => (
+                    <li key={habit.id}>
+                        {habit.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )}
+    </>
   )
 }
 
